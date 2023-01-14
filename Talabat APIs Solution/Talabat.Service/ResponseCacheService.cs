c4c656a01d9c7f797893535daceeee991ed797a3
yousef osama <yousefosama654@gmail.com>
2023-01-14 03:15:06 +0200

    create chaching service

---
+++ Talabat APIs Solution/Talabat.Service/ResponseCacheService.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http.Json;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Talabat.core.Servicecs;
+
+namespace Talabat.Service
+{
+    public class ResponseCacheService : IResponseCacheService
+    {
+        private readonly IDatabase db;
+
+        public ResponseCacheService(IConnectionMultiplexer redis)
+        {
+            this.db = redis.GetDatabase();
+        }
+
+        public IConnectionMultiplexer Redis { get; }
+
+        public async Task CachResponseAsync(string cacheKey, object response, TimeSpan timeToLive)
+        {
+            if (response == null)
+            {
+                return;
+            }
+            var JsonOptions = new JsonSerializerOptions() { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+            var serilizedResponse = JsonSerializer.Serialize(response, JsonOptions);
+            await this.db.StringSetAsync(cacheKey, serilizedResponse, timeToLive);
+        }
+
+        public async Task<string> GetCachedResponseAsync(string cacheKey)
+        {
+            var response = await this.db.StringGetAsync(cacheKey);
+            if (response.IsNullOrEmpty)
+            {
+                return null;
+            }
+            return response;
+        }
+    }
+}
---
+++ Talabat APIs Solution/Talabat.core/Servicecs/IResponseCacheService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talabat.core.Servicecs
+{
+    public interface IResponseCacheService
+    {
+        Task CachResponseAsync(string cacheKey, object response,TimeSpan timeToLive);
+
+        Task<string> GetCachedResponseAsync(string cacheKey);
+    }
+}
